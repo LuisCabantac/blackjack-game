735f75395ced4a8f77f150c447777c24b1b4dd2a
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-09 06:24:17 +0800

    Added an if/else statement to determine if the sum is less or equal to 20, or if it's exactly 21, or else the player is out of the game.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 let firstCard = 1;
 let secondCard = 1;
 let sum = firstCard + secondCard;
+
+if (sum <= 20) {
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21) {
+    console.log("You've got a Blackjack!");
+} else {
+    console.log("You're out of the game!");
+}
