b28ae0b6388a21584ddf855d50954c7aecda6c60
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-09 07:20:34 +0800

    Declared a message variable with an empty string value and reassigned it inside the if/else statement where the console logs were and switched to it instead

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let secondCard = 1;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
+let message = "";
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card?");
+    message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-    console.log("You've got a Blackjack!");
+    message = "You've got a Blackjack!";
     hasBlackJack = true;
 } else {
-    console.log("You're out of the game!");
+    message = "You're out of the game!";
     isAlive = false;
 }
