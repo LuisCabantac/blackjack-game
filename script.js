3b334db8f04444ac028ac7e7df111ad146ebcf12
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-09 06:34:50 +0800

    Declared a hasBlackJack variable and by default the value is false, until the player gets a blackjack it'll be reassigned to true

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let firstCard = 1;
 let secondCard = 1;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
 
 if (sum <= 20) {
     console.log("Do you want to draw a new card?");
 } else if (sum === 21) {
     console.log("You've got a Blackjack!");
+    hasBlackJack = true;
 } else {
     console.log("You're out of the game!");
 }
