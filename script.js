3581b81bd8dc6987b544975d3ebb549f5a6e764d
LuisCabantac <151472373+LuisCabantac@users.noreply.github.com>
2024-03-09 08:24:05 +0800

    Added startGame() function and nested the if/else statement inside of the function

--- script.js
+++ script.js
@@ -5,12 +5,14 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 
-if (sum <= 20) {
-    message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-    message = "You've got a Blackjack!";
-    hasBlackJack = true;
-} else {
-    message = "You're out of the game!";
-    isAlive = false;
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?";
+    } else if (sum === 21) {
+        message = "You've got a Blackjack!";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game!";
+        isAlive = false;
+    }
 }
